2fe3c05b24631ffba2ea678e5c96e6f5728b9ac0
= <laherasif@gmail.com>
2022-01-27 20:48:51 +0500

    add api info

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ import NewDetail from './components/NewDetail'
 import Main from './components/Mian'
 import './App.css'
 const App = () => {
+  // api from alan project
   const api = "7284e004712265cd59071cb9497eff412e956eca572e1d8b807a3e2338fdd0dc/stage"
+
   const [alanInstanc, setAlanInstance] = useState()
   const [newHeadlines, setNewHeadlinese] = useState([])
   useEffect(() => {
